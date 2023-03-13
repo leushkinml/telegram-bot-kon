43dee6cca0046829bc47c0d23956557f1bb75404
leush <leushkinml@mail.ru>
2023-03-13 18:00:01 +0300

    Initial commit

---
+++ src/main/java/com/skypro/telegrambotkon/TelegramBotKonApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.telegrambotkon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotKonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotKonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/telegrambotkon/TelegramBotKonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.telegrambotkon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotKonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
